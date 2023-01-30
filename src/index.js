e17e9a8088d20121f17b00e44c9dc6f355151b8f
Djordje Mirosavljevic <djordje.mirosavljevic@dci.education>
2023-01-30 15:15:41 +0100

    komit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 import "./css/style.css"; //importing css
+const modalBox = document.querySelector(".modal");
+const modal = () => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("block");
+    }, 3000);
+  });
+};
+const section1 = document.querySelector("#myModal");
+section1.classlist.add(".modal__content");
+modal().then((result) => (modalBox.style.display = result));
